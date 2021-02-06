2ae8bb5df5ee3e461630d9c5d3aa2e1cb8922599
stavroskasidis <stavroskasidis@outlook.com>
2021-02-06 19:14:29 +0200

    Preperation for template packaging/Updated template to .net 5

---
+++ template/BlazorWithIdentity.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using BlazorWithIdentity.Client.Services.Contracts;
+using BlazorWithIdentity.Client.Services.Implementations;
+using BlazorWithIdentity.Client.States;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorWithIdentity.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<IdentityAuthenticationStateProvider>();
+            builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<IdentityAuthenticationStateProvider>());
+            builder.Services.AddScoped<IAuthorizeApi, AuthorizeApi>();
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+            var host = builder.Build();
+            await host.RunAsync();
+        }
+    }
+}
